f68ff15ee15c9b2f1ea7ae74ed067d48bfb0fb6d
Sikander Ali Tariq <89202153+sikandertariq@users.noreply.github.com>
2024-06-24 01:48:33 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigation";
+import AppNavigation from "./src/navigation/AppNavigation";
+
+export default function App() {
+  return <AppNavigation />;
+}
